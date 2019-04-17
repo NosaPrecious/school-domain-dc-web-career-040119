207c4303893ab13009058194c31b909070cf98d2
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-17 03:44:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -33,10 +33,7 @@ class School
   end
   
   def sort
-    @roster.map do |grade, name|
-      name.sort!
-    end 
-    #@roster.sort_by{|grade,name| grade}
+    @roster.sort_by{|k,v| [k,v]}
     binding.pry
   end
   
